226a1104a37a41bea467b5598aa2f082b2df9f26
estacioj91 <estacioj91@yahoo.com>
2018-09-21 17:12:24 -0700

    Carousel and Cards added
    added carousel and cards to display test info

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
 });
+//inits carousel
+document.addEventListener('DOMContentLoaded', function () {
+    //returns array of elements that match selector
+    var elems = document.querySelectorAll('.carousel');
+    //inits all elements of the elems array with options
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
+    //sets interval so images scroll at the interval
+    setInterval(function () {
+        //selects first instance, might need to be changed later
+        instances[0].next();
+    }, 3000);
+});
